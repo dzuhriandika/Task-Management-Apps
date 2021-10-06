85201f4ee20b29cc656f885f0cc70e30a0064d81
dzuhriandika <dzuhriandika@gmail.com>
2021-10-06 11:51:51 +0700

    fix baseURL

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -2,11 +2,11 @@ import 'package:flutter_auth/model/task.dart';
 import 'package:http/http.dart' show Client;
 
 class ApiService {
-  final String baseUrl = "https://api.sheety.co/39ed35af0f13a0788bddf4a354c3b0fc_raw=1";
+  final String baseUrl = "https://api.sheety.co/39ed35af0f13a0788bddf4a354c3b0fc?_raw=1";
   Client client = Client();
 
   Future<List<Task>> getTasks() async {
-    final response = await client.get("https://sheet.best/api/sheets/d4fcab00-e8a9-4267-bcc0-f192b5efc57d_raw=1");
+    final response = await client.get("https://sheet.best/api/sheets/d4fcab00-e8a9-4267-bcc0-f192b5efc57d?_raw=1");
     if (response.statusCode == 200) {
       return taskFromJson(response.body);
     } else {
@@ -15,7 +15,7 @@ class ApiService {
   }
     Future<bool> createTask(Task data) async {
     final response = await client.post(
-      "https://sheet.best/api/sheets/d4fcab00-e8a9-4267-bcc0-f192b5efc57d_raw=1",
+      "https://sheet.best/api/sheets/d4fcab00-e8a9-4267-bcc0-f192b5efc57d?_raw=1",
       headers: {"content-type": "application/json"},
       body: taskToJson(data),
     );
